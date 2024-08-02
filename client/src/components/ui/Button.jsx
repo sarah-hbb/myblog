c9a3d877f19a3a9d351994a3fe9159f0c9234e47
sarah-hbb <sarahabibi.89@gmail.com>
2024-08-02 20:35:35 +0200

    Refine Button component

--- client/src/components/ui/Button.jsx
+++ client/src/components/ui/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({
   type,
   size,
   inverseColor,
+  deleteBtn,
   onClick,
   children,
   className,
@@ -14,10 +15,13 @@ const Button = ({
       disabled={disabled}
       type={type}
       onClick={onClick}
-      className={`${className} flex items-center justify-center p-2 text-center rounded-md hover:font-semibold focus:font-semibold 
-        hover:border-2 focus:border-2 border-cyan-600  ${
+      className={`${className} flex items-center justify-center p-2 text-center rounded-md 
+      hover:font-semibold focus:font-semibold 
+        hover:border-1 focus:border-1 border-cyan-600 ${
           inverseColor
             ? "bg-cyan-100 text-cyan-900 hover:bg-black hover:text-white"
+            : deleteBtn
+            ? "bg-red-400 border-none hover:bg-red-600 text-black"
             : "bg-gradient-to-r from-cyan-100 to-cyan-900 text-black"
         } `}
     >

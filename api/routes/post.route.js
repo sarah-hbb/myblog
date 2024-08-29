c2f55ded1de034003dcfffc5cc04846edab1db50
sarah-hbb <sarahabibi.89@gmail.com>
2024-08-29 14:21:13 +0200

    Create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const create = require("../controllers/post.controller.js");
+const { create, getPosts } = require("../controllers/post.controller.js");
 const verifyToken = require("../utils/verifyUser.js");
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/getposts", getPosts);
 
 module.exports = router;

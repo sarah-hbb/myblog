a074cd18d029bfe148c4e2ec5a50c835bb018617
sarah-hbb <sarahabibi.89@gmail.com>
2024-09-02 23:20:15 +0200

    Add delete post functionality to Dashboard posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { create, getPosts } = require("../controllers/post.controller.js");
+const {
+  create,
+  getPosts,
+  deletepost,
+} = require("../controllers/post.controller.js");
 const verifyToken = require("../utils/verifyUser.js");
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 
 module.exports = router;
--- client/src/components/dashboard/DashboardPosts.jsx
+++ client/src/components/dashboard/DashboardPosts.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import PostsList from "./PostsList";
+import { Link } from "react-router-dom";
+import Modal from "../ui/Modal";
+import Button from "../ui/Button";
+import { CiWarning } from "react-icons/ci";
 
 const DashboardPosts = () => {
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [postIdToDelete, setPostIdToDelete] = useState("");
 
   const { currentUser } = useSelector((state) => state.user);
 
@@ -43,19 +48,104 @@ const DashboardPosts = () => {
     } catch (error) {}
   };
 
+  const handleDeletePost = async () => {
+    setShowDeleteModal(false);
+    try {
+      const res = await fetch(
+        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUserPosts((prv) =>
+          prv.filter((post) => post._id !== postIdToDelete)
+        );
+      }
+    } catch (error) {}
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <PostsList posts={userPosts} />
-      {showMore && (
-        <button
-          type="button"
-          className="font-bold text-lg text-cyan-600 p-4 self-center
+      <div
+        className="flex flex-col items-center gap-1 p-2 lg:max-w-7xl
+      lg:mx-auto lg:p-10 lg:shadow-2xl
+      [&>*:not(:last-child)]:border-b [&>*]:border-gray-300 last:border-b-0"
+      >
+        {userPosts.map((post, index) => (
+          <div
+            key={index}
+            className="flex justify-between gap-3 p-2 min-w-full
+          hover:scale-105 transition-all"
+          >
+            <Link to={`/post/${post.slug}`} className="flex-1 flex gap-3 p-1">
+              <img src={post.image} alt="" className="h-20 w-20 object-cover" />
+              <div className="flex-1 flex flex-col lg:flex-row justify-between lg:gap-9">
+                <h1 className="flex-1 text-lg uppercase font-semibold">
+                  {post.title}
+                </h1>
+                <h3>{post.category}</h3>
+                <span className="text-gray-500">
+                  Last updated at{" "}
+                  {new Date(post.updatedAt).toLocaleDateString()}
+                </span>
+              </div>
+            </Link>
+            <div className="flex flex-row gap-4">
+              <Link to={`/update-post/${post._id}`}>
+                <h3 className="text-cyan-700 hover:font-semibold">Edit</h3>
+              </Link>
+
+              <h3
+                className="text-red-600 cursor-pointer hover:font-semibold"
+                onClick={() => {
+                  setShowDeleteModal(true);
+                  setPostIdToDelete(post._id);
+                }}
+              >
+                Delete
+              </h3>
+            </div>
+          </div>
+        ))}
+        {showMore && (
+          <button
+            type="button"
+            className="font-bold text-lg text-cyan-600 p-4 self-center
           hover:text-black hover:scale-105 transition-all"
-          onClick={handleShowMore}
-        >
-          Show more posts
-        </button>
-      )}
+            onClick={handleShowMore}
+          >
+            Show more posts
+          </button>
+        )}
+        {showDeleteModal && (
+          <Modal
+            onClose={() => {
+              setShowDeleteModal(false);
+            }}
+          >
+            <CiWarning className="text-5xl" />
+            <h2>Are you sure you want to delete this post</h2>
+            <div className="flex flex-row gap-5">
+              <Button deleteBtn={true} onClick={handleDeletePost} type="button">
+                Yes, delete this post
+              </Button>
+              <Button
+                inverseColor={true}
+                onClick={() => {
+                  setShowDeleteModal(false);
+                }}
+                type="button"
+              >
+                No, cancel
+              </Button>
+            </div>
+          </Modal>
+        )}
+      </div>
     </div>
   );
 };

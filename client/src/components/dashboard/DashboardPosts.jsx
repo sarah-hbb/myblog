2bc422e4e950455c93e297871dad382943b076ed
sarah-hbb <sarahabibi.89@gmail.com>
2024-09-02 20:37:25 +0200

    Add sho more posts functionality to the dashboard posts

--- client/src/components/dashboard/DashboardPosts.jsx
+++ client/src/components/dashboard/DashboardPosts.jsx
@@ -4,9 +4,9 @@ import PostsList from "./PostsList";
 
 const DashboardPosts = () => {
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
   const { currentUser } = useSelector((state) => state.user);
-  console.log(userPosts);
 
   const fetchPosts = async () => {
     try {
@@ -14,6 +14,9 @@ const DashboardPosts = () => {
       const data = await res.json();
       if (res.ok) {
         setUserPosts(data.posts);
+        if (data.posts.lenght < 9) {
+          setShowMore(false);
+        }
       }
     } catch (error) {}
   };
@@ -24,9 +27,35 @@ const DashboardPosts = () => {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prv) => [...prv, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {}
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <PostsList posts={userPosts} />
+      {showMore && (
+        <button
+          type="button"
+          className="font-bold text-lg text-cyan-600 p-4 self-center
+          hover:text-black hover:scale-105 transition-all"
+          onClick={handleShowMore}
+        >
+          Show more posts
+        </button>
+      )}
     </div>
   );
 };

c5d30ee1b49dbffb7067d5fd461dc7b62fbd1b1f
sarah-hbb <sarahabibi.89@gmail.com>
2024-10-02 18:29:00 +0200

    Add last month posts to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,39 +28,47 @@ const Home = () => {
   return (
     <div className="w-full mx-auto">
       <Carousel posts={posts} />
-      <div className="mb-4 border border-slate-200"></div>
-      {lastMonthPosts && (
-        <div className="w-full max-w-lg flex flex-col p-4">
-          <h1 className="text-2xl font-bold text-cyan-700 p-3 italic">
-            Last month posts
-          </h1>
-          {lastMonthPosts.map((post, index) => (
-            <div
-              key={index}
-              className="flex justify-between gap-3 p-2 min-w-full
-          hover:scale-105 transition-all"
+      <div className="mb-4 border border-slate-200 "></div>
+      <div className="flex md:flex-col p-3 w-full mx-auto">
+        {lastMonthPosts && (
+          <div className="w-full max-w-lg flex flex-col p-4 border shadow-lg">
+            <h1
+              className="text-xl font-bold text-cyan-700 p-3 italic uppercase
+           bg-gray-200 rounded"
             >
-              <Link to={`/post/${post.slug}`} className="flex-1 flex gap-3 p-1">
-                <img
-                  src={post.image}
-                  alt=""
-                  className="h-20 w-20 object-cover"
-                />
-                <div className="flex-1 flex flex-col  justify-between ">
-                  <h1 className="flex-1 text-lg uppercase font-semibold">
-                    {post.title}
-                  </h1>
-                  <h3>{post.category}</h3>
-                  <span className="text-gray-500">
-                    Last updated at{" "}
-                    {new Date(post.updatedAt).toLocaleDateString()}
-                  </span>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
+              Last month posts
+            </h1>
+            {lastMonthPosts.map((post, index) => (
+              <div
+                key={index}
+                className="flex justify-between gap-3 p-2 min-w-full
+          hover:scale-105 transition-all"
+              >
+                <Link
+                  to={`/post/${post.slug}`}
+                  className="flex-1 flex gap-3 p-1"
+                >
+                  <img
+                    src={post.image}
+                    alt=""
+                    className="h-20 w-20 object-cover"
+                  />
+                  <div className="flex-1 flex flex-col  justify-between ">
+                    <h1 className="flex-1 text-lg uppercase font-semibold">
+                      {post.title}
+                    </h1>
+                    <h3>{post.category}</h3>
+                    <span className="text-gray-500">
+                      Last updated at{" "}
+                      {new Date(post.updatedAt).toLocaleDateString()}
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };

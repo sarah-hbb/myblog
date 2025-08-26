a0a998fc04626fe39b3d838a8ae525193242ac8e
sarah-hbb <sarahabibi.89@gmail.com>
2025-08-26 14:28:50 +0200

    Add dummy description to About page

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./header/Header";
 import Footer from "./Footer";
 
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="p-20">
+      <h1>About is under updates...</h1>
+    </div>
+  );
 };
 
 export default About;
